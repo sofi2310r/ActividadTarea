c9cb38c6dd0d676d3d1964ecb87aa5559985613f
felipearandia24-hue <felipearandia24@gmail.com>
2026-07-15 22:24:03 -0500

    Supabase

---
+++ js/supabase-config.js
@@ -0,0 +1,6 @@
+
+const SUPABASE_URL = "https://supabase.com/dashboard/project/oexxtfndncehgfjvybta/settings/api-keys";
+const SUPABASE_ANON_KEY = "sb_publishable_c3tsVh9mhJBkqPBY0mty-A_FaHH-IyK";
+
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
