b82d8f7f463fd713361fa37e16fb7a859734dd4a
felipearandia24-hue <felipearandia24@gmail.com>
2026-07-15 22:26:39 -0500

    change url

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,5 +1,5 @@
 
-const SUPABASE_URL = "https://supabase.com/dashboard/project/oexxtfndncehgfjvybta/settings/api-keys";
+const SUPABASE_URL = "https://oexxtfndncehgfjvybta.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_c3tsVh9mhJBkqPBY0mty-A_FaHH-IyK";
 
 
